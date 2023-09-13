65a852ad4a0cf84b6bd9a54a47d16d10dcb3c0db
lucky-blessed <blessednkwor7@gmail.com>
2023-09-12 19:08:26 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Prints the alphabet 10 times
+ */
+ void print_alphabet_x10(void)
+ {
+	 char letter;
+	 int i;
+
+	 for (i = 0; i <= 9; i++)
+	 {
+		 letter = 'a';
+		 while (letter <= 'z')
+		 {
+			 _putchar(letter);
+			 letter++;
+		 }
+		 _putchar('\n');
+	 }
+ }
