64111adfe13704bccec4ab4e802efac81d2328b9
lucky-blessed <blessednkwor7@gmail.com>
2023-09-13 23:16:46 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,15 +4,15 @@
  * @n: number whose last digit is to be calculated
  * Return: Returns the last digit of n
  */
-int _print_last_digit(int n)
+int print_last_digit(int n)
 {
 	int last_digit;
 
+	last_digit = n % 10;
 	if (n < 0)
 	{
-		n = -n;
+		last_digit = last_digit * -1;
 	}
-	last_digit =  n % 10;
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
