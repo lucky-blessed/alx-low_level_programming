836cdf7079d76ee064eafef4d8591b1743155179
lucky-blessed <blessednkwor7@gmail.com>
2023-09-14 01:47:32 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,26 +4,32 @@
  */
 void times_table(void)
 {
-	int row, col, product;
+	int row, col, product, ten, one;
 
 	for (row = 0; row <= 9; row++)
 	{
 		for (col = 0; col <= 9; col++)
 		{
 			product = row * col;
+			ten = product / 10;
+			one = product % 10;
 			if (col == 0)
 			{
-				_putchar(product + '0');
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(one + '0');
 			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (product < 10)
-				{
-					_putchar(' ');
-				}
-				_putchar(product + '0');
+				_putchar(ten + '0');
+				_putchar(one + '0');
 			}
 		}
 		_putchar('\n');
