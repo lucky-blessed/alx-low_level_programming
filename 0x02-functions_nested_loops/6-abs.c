e2f10723fdadc11ff29839e29382b12dd33bf99f
lucky-blessed <blessednkwor7@gmail.com>
2023-09-13 17:01:13 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Prints absolute value of a number
+ * @n: The number to be printed to print its absolute value
+ * Return: Absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
