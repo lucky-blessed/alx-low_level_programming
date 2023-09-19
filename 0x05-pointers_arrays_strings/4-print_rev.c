9072a8303ddf734957dc5827bd2b3b7747215f2c
lucky-blessed <blessednkwor7@gmail.com>
2023-09-19 14:00:13 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - Prints strings in reverse order
+ * @s: strings variable to be printed
+ */
+void print_rev(char *s);
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (int i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
