2e953c52ad0a08da9182d4ce15b1bdefcaff198c
lucky-blessed <blessednkwor7@gmail.com>
2023-09-21 11:15:56 -0700

    Copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - Function that copies a string
+ * @dest: Destination to copy string to
+ * @src: source of strings to copy
+ * @n: Available bytes in dest
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len_dest = 0;
+	int i = 0;
+
+	while (dest[len_dest] != '\0')
+	{
+		len_dest++;
+	}
+	while (src[i] != '\0' && i < n)
+	{
+		dest[len_dest] = src[i];
+		i++;
+	}
+	dest[len_dest] = '\0';
+	return (dest);
+}
