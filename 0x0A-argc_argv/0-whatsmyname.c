eb85c5022bc6c118d46fb3adc38acadaccedba84
lucky-blessed <blessednkwor7@gmail.com>
2023-10-07 17:43:45 -0700

    Function that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: The number of commandline arguments
+ * @argv: An array containing the command line argument.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+int _putchar(char c);
+int main(int argc, char *argv[]);
+#endif
